9b52f66277bd53210be8e1ad6321f91fd193e696
Arman.mrn <arman.mrn@yahoo.com>
2022-01-17 15:47:15 +0330

    2022/1/17 3:47 PM time added

---
+++ src/main/java/com/example/repeatlearn/build/Time.java
@@ -0,0 +1,35 @@
+package com.example.repeatlearn.build;
+
+import java.util.Date;
+
+public class Time{
+
+    private static final int k = 1000 * 60 * 60 * 24;
+
+    public long getToday(){
+        Date date = new Date();
+        return date.getTime()/k;
+    }
+
+    public long getLevel_1_day(){
+        return getToday()+1;
+    }
+
+    public long getLevel_2_day(){
+        return getToday()+3;
+    }
+
+    public long getLevel_3_day(){
+        return getToday()+7;
+    }
+
+    public long getLevel_4_day(){
+        return getToday()+15;
+    }
+
+    public long getLevel_5_day(){
+        return getToday()+30;
+    }
+
+
+}
